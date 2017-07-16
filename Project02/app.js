9025a160b07b864232bf4a95285de358d0e9db6d
nlivaic <nlivaic@gmail.com>
2017-07-16 21:06:08 +0200

    Type: feat
    Explicitly defining array: any[] and string[].

--- Project02/app.js
+++ Project02/app.js
@@ -11,4 +11,14 @@ var hasHobbies = false;
 // assign types explicitly
 var myRealAge;
 myRealAge = 33;
-// myRealAge = 'Max'; 
+// myRealAge = 'Max';
+// array any
+var bunchOfStuff = ["Book", 100, "Bike"];
+console.log(bunchOfStuff[0]);
+bunchOfStuff = [45, "Airplane"];
+console.log(bunchOfStuff[0]);
+console.log(typeof bunchOfStuff);
+// array strings
+var hobbies = ["Cooking", "Sports"];
+hobbies = ["Some hobby"];
+console.log(hobbies[0]);
--- Project02/app.ts
+++ Project02/app.ts
@@ -1,16 +1,28 @@
 // string
-let myName : string = 'Max';
+let myName: string = 'Max';
 // myName = 1;
 
 // number
-let age : number = 33;
+let age: number = 33;
 // age = 'Max';
 
 // boolean
-let hasHobbies : boolean = false;
+let hasHobbies: boolean = false;
 // hasHobbies = 1;
 
 // assign types explicitly
-let myRealAge : number;
+let myRealAge: number;
 myRealAge = 33;
 // myRealAge = 'Max';
+
+// array any
+let bunchOfStuff: any[] = ["Book", 100, "Bike"];
+console.log(bunchOfStuff[0]);
+bunchOfStuff = [45, "Airplane"]
+console.log(bunchOfStuff[0]);
+console.log(typeof bunchOfStuff);
+
+// array strings
+let hobbies: string[] = ["Cooking", "Sports"];
+hobbies = ["Some hobby"];
+console.log(hobbies[0]);
